eada5a119276b2f120fcefb8da156b92dd052dc8
Junmoxiao <1139560172@qq.com>
2019-11-27 09:59:08 +0800

    Update main.ts

--- vue路由封装/main.ts
+++ vue路由封装/main.ts
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import '@/assets/css/reset.scss';
-import 'element-ui/lib/theme-chalk/index.css';
-import api from './request';
 
-Vue.prototype.$api = api;
-Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
